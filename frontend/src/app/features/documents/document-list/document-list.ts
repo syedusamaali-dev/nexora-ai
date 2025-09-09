adac4920597b341a9012e2990a458a504658825e
Usama2201 <usamaali191@gmail.com>
2025-09-09 12:30:00 +0500

    AI folder Creatiion

---
+++ frontend/src/app/features/documents/document-list/document-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-list',
+  imports: [],
+  templateUrl: './document-list.html',
+  styleUrl: './document-list.scss',
+})
+export class DocumentList {
+
+}
---
+++ frontend/src/app/features/documents/document-upload/document-upload.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-upload',
+  imports: [],
+  templateUrl: './document-upload.html',
+  styleUrl: './document-upload.scss',
+})
+export class DocumentUpload {
+
+}
