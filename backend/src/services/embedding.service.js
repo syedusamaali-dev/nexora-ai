b15dbb71e042aaa766272f5028607a607c8e1c07
Usama2201 <usamaali191@gmail.com>
2025-10-03 12:30:00 +0500

    Update Gemini Ai for Embedding

--- backend/src/services/embedding.service.js
+++ backend/src/services/embedding.service.js
@@ -2,7 +2,13 @@ import { GoogleGenAI } from '@google/genai';
 
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY
-});
+  
+},
+console.log(
+  'Gemini key loaded:',
+  Boolean(process.env.GEMINI_API_KEY),
+  process.env.GEMINI_API_KEY?.slice(0, 8)
+));
 
 export const createEmbedding = async (text) => {
   const response = await ai.models.embedContent({
@@ -11,4 +17,9 @@ export const createEmbedding = async (text) => {
   });
 
   return response.embeddings[0].values;
+  console.log(
+  'Gemini key loaded:',
+  Boolean(process.env.GEMINI_API_KEY),
+  process.env.GEMINI_API_KEY?.slice(0, 8)
+);
 };
